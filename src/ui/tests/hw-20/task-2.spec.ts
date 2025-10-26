d8a32604c5ca6f7d71bc36ec884fa08f62bfb37e
AlbertAlexeenco <alick1777@gmail.com>
2025-10-26 08:43:58 +0200

    hw-20 task 2

--- src/ui/tests/hw-20/task-2.spec.ts
+++ src/ui/tests/hw-20/task-2.spec.ts
@@ -6,3 +6,38 @@
 
 //   Рекоммендации:
 //   - Для доступа к localStorage используйте https://playwright.dev/docs/evaluating
+
+import test, { expect } from "@playwright/test";
+
+interface ICredentials {
+    username: string;
+    password: string;
+}
+
+test.describe("[anatoly-karpovich site] [Login form]", () => {
+
+    const validCredentials: ICredentials = {
+        username: "test@gmail.com",
+        password: "SecretPw123!@#"
+    }
+
+    const url = "https://anatoly-karpovich.github.io/demo-login-form/";
+
+    test("Login with credentials from localStorage", async ({ page })=>{
+        
+        const usernameInput = page.locator("#userName");
+        const passwordInput = page.locator("#password");
+        const submitBtn = page.locator("#submit");
+        const successfullMessage = page.locator("#successMessage");
+        
+        await page.goto(url);
+        await page.evaluate((credentials) => {
+            localStorage.setItem(credentials.username, JSON.stringify({ name: credentials.username, password: credentials.password }));
+        }, validCredentials);
+
+        await usernameInput.fill(validCredentials.username);
+        await passwordInput.fill(validCredentials.password);
+        await submitBtn.click();
+        await expect(successfullMessage).toHaveText(`Hello, ${validCredentials.username}!`);
+        })
+})
